ef8dc888336cef0220f35f14ffc6550e6f90e7e2
Ted Nichols <foster.nichols@gmail.com>
2021-04-11 18:18:55 -0400

    fix typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Dilemma } from './Game';
 import { DilemmaBoard } from './Board';
 import { SocketIO } from 'boardgame.io/multiplayer'
 
-const DilemmaClient = Client({ numPlayers: 1, game: Dilemma , board: DilemmaBoard multiplayer: SocketIO({server: true ? 'https://pandemic-legacy0.herokuapp.com' : 'localhost:53954'})});
+const DilemmaClient = Client({ numPlayers: 1, game: Dilemma , board: DilemmaBoard, multiplayer: SocketIO({server: true ? 'https://pandemic-legacy0.herokuapp.com' : 'localhost:53954'})});
 
 const App = () => (
   <div>
