a79fd7b86265f397c9362cec20a0f0b6c61d9bc1
Ted Nichols <foster.nichols@gmail.com>
2021-05-23 19:24:48 -0400

    oops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Client } from 'boardgame.io/react';
 import { Dilemma } from './Game';
 import { DilemmaBoard } from './Board';
-// import { SocketIO } from 'boardgame.io/multiplayer'
+import { SocketIO } from 'boardgame.io/multiplayer'
 
-const DilemmaClient = Client({ numPlayers: 1, game: Dilemma , board: DilemmaBoard});//, multiplayer: SocketIO({server: true ? 'https://pandemic-legacy0.herokuapp.com' : 'localhost:53954'})});
+const DilemmaClient = Client({ numPlayers: 1, game: Dilemma , board: DilemmaBoard, multiplayer: SocketIO({server: true ? 'https://pandemic-legacy0.herokuapp.com' : 'localhost:53954'})});
 
 const App = () => (
   <div>
